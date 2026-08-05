0876b59dce767d9f2cff19a495ae98072274c56c
wehbemartinezf-gif <wehbemartinezf@gmail.com>
2026-08-04 20:48:04 -0400

    Crear players.js

---
+++ js/js/players.js
@@ -0,0 +1,63 @@
+const PLAYERS = [
+
+{
+id:1,
+name:"Luis Martínez",
+pos:"PG",
+age:23,
+ovr:84,
+pot:91,
+salary:850000,
+energy:100,
+morale:90
+},
+
+{
+id:2,
+name:"Carlos Reyes",
+pos:"SG",
+age:26,
+ovr:81,
+pot:84,
+salary:780000,
+energy:100,
+morale:92
+},
+
+{
+id:3,
+name:"Miguel Santos",
+pos:"SF",
+age:22,
+ovr:86,
+pot:95,
+salary:1200000,
+energy:100,
+morale:95
+},
+
+{
+id:4,
+name:"José Pérez",
+pos:"PF",
+age:29,
+ovr:80,
+pot:81,
+salary:900000,
+energy:100,
+morale:88
+},
+
+{
+id:5,
+name:"Robert King",
+pos:"C",
+age:27,
+ovr:89,
+pot:90,
+salary:1600000,
+energy:100,
+morale:97
+}
+
+];
